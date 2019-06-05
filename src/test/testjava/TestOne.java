e97e7620d8a7517a2900bcb2cc6bff7e22be0f68
shajin1982 <179682068@qq.com>
2019-06-05 17:34:17 +0800

    dz

---
+++ src/test/testjava/TestOne.java
@@ -0,0 +1,32 @@
+package testjava;
+
+import com.ssm.hq.model.QyWeiXin;
+import com.ssm.hq.support.HttpRequest;
+import com.ssm.hq.support.WeiXinFactory;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.jdom.JDOMException;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class TestOne {
+    @Test
+    public void test3() throws IOException, JDOMException, DocumentException, ParseException {
+        WeiXinFactory wf=new WeiXinFactory();
+    }
+}
