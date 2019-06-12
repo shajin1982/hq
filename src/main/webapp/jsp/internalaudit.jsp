a684668645572c29098060a09617632f714c1d21
shajin1982 <179682068@qq.com>
2019-06-12 10:18:37 +0800

    123

--- src/main/webapp/jsp/internalaudit.jsp
+++ src/main/webapp/jsp/internalaudit.jsp
@@ -195,7 +195,7 @@
                                                 <dd><a href="">选项3</a></dd>
                                             </dl>
                                         </li>
-                                        <li class="layui-nav-item"><a href="demo2">季度评审汇总报告</a></li>
+                                        <li class="layui-nav-item"><a href="demo">季度评审汇总报告</a></li>
                                         <li class="layui-nav-item">
                                             <a href="javascript:;">季度定期评定报告</a>
                                             <dl class="layui-nav-child">
--- src/test/testjava/TestOne.java
+++ src/test/testjava/TestOne.java
@@ -1,39 +1,31 @@
 package testjava;
 
-import com.ssm.hq.model.CustomPostpone;
-import com.ssm.hq.model.QyWeiXin;
-import com.ssm.hq.service.CustomPostponeService;
-import com.ssm.hq.support.HttpRequest;
-import com.ssm.hq.support.MultipleDataSource;
-import com.ssm.hq.support.WeiXinFactory;
-import org.dom4j.Document;
+import com.ssm.hq.model.User;
+import com.ssm.hq.service.UserService;
+import com.ssm.hq.service.impl.UserServiceImpl;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.SAXReader;
-import org.dom4j.io.XMLWriter;
 import org.jdom.JDOMException;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
-import javax.annotation.Resource;
 
 public class TestOne {
-    @Resource
-    private CustomPostponeService customPostponeService;
+
+    private UserService userService;
+    @Before
+    public void getBefore(){
+        String xmlPath = "target/hq/WEB-INF/classes/spring-mybatis.xml";
+        ApplicationContext ac = new FileSystemXmlApplicationContext(xmlPath);
+        userService = ac.getBean(UserServiceImpl.class);
+    }
+
     @Test
     public void test3() throws IOException, JDOMException, DocumentException, ParseException {
-        List<CustomPostpone> list=customPostponeService.getAllCustomPostpone();
+        List<User> userList = userService.getAllUser();
     }
 }
